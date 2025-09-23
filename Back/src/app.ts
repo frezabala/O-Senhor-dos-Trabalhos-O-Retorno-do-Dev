e8965b8846e05825525b8f02225dcc77d8601fd0
NATHALIA MINOSSI OLIVEIRA <04987029014@senacrs.edu.br>
2025-09-23 13:46:53 -0300

    update

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -2,11 +2,23 @@ import express from 'express'
 import { config } from 'dotenv'
 import routes from './routes'
 import cors from "cors";
+import path from 'path';
 
 config()
 const app = express()
+
 app.use(cors())
 app.use(express.json())
+
+// Servir arquivos estáticos da pasta src/public 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Rota raiz: abre a página home.html 
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pages', 'home.html'))
+})
+
+
 app.use(routes)
 
 export default app

984038abf7b0d7322c455d1e27025f9bca0edae8
talita <talitanaibert.c@gmail.com>
2025-09-23 14:34:30 -0300

    comentar

--- script/btn.js
+++ script/btn.js
@@ -1,17 +1,5 @@
 // Quando o usuário clicar no botão "Iniciar o Jogo"
-document.getElementById('btnStart').addEventListener('click', function() {
+document.getElementById('start-game').addEventListener('click', function() {
     // Redireciona para a página do jogo
     window.location.href = 'game.html';
-});
-
-// Quando o usuário clicar no botão "Ranking"
-document.getElementById('ranking').addEventListener('click', function() {
-    // Redireciona para a página de ranking
-    window.location.href = 'ranking.html';
-});
-
-// Quando o usuário clicar no botão "Conquistas"
-document.getElementById('conquistas').addEventListener('click', function() {
-    // Redireciona para a página de conquistas
-    window.location.href = 'conquistas.html';
 });

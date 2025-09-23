55bbe613599b14bd86b9477d6fcd73a7e9297d61
talita <talitanaibert.c@gmail.com>
2025-09-23 15:16:51 -0300

    botao config

--- script/btn.js
+++ script/btn.js
@@ -3,3 +3,14 @@ document.getElementById('start-game').addEventListener('click', function() {
     // Redireciona para a página do jogo
     window.location.href = 'game.html';
 });
+
+
+// Quando o botão #hi for clicado
+document.getElementById("hi").addEventListener("click", function() {
+  document.querySelector(".layer").classList.add("clicked");
+});
+
+// Quando o ícone #remove for clicado
+document.getElementById("remove").addEventListener("click", function() {
+  document.querySelector(".layer").classList.remove("clicked");
+});
